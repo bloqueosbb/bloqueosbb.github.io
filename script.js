980faa1803040f2c06183c3473e1ca55fa34c086
bloqueosbb <140268120+bloqueosbb@users.noreply.github.com>
2023-07-25 14:38:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,141 +1,200 @@
-
-    // Evento de clic para los botones de las consultas
-    document.getElementById('btn-gca').addEventListener('click', () => copyToClipboard('sudo su postgres\npsql produccion_lpa\nselect * from migracion.ver_bloqueos ;\n'));
-    document.getElementById('btn-pmi').addEventListener('click', () => copyToClipboard('sudo su postgres\npsql produccion_pmi\nselect * from migracion.ver_bloqueos ;\n'));
-    document.getElementById('btn-tfe').addEventListener('click', () => copyToClipboard('sudo su postgres\npsql produccion_tfe\nselect * from migracion.ver_bloqueos ;\n'));
-    document.getElementById('btn-lza').addEventListener('click', () => copyToClipboard('sudo su postgres\npsql produccion_lza\nselect * from migracion.ver_bloqueos ;\n'));
-    document.getElementById('btn-val').addEventListener('click', () => copyToClipboard('sudo su postgres\npsql produccion_val\nselect * from migracion.ver_bloqueos ;\n'));
-    document.getElementById('btn-sdq').addEventListener('click', () => copyToClipboard('sudo su postgres\npsql sd_produccion\nselect * from migracion.ver_bloqueos ;\n'));
-    document.getElementById('btn-pur').addEventListener('click', () => copyToClipboard('sudo su postgres\npsql produccion_pr\nselect * from migracion.ver_bloqueos ;\n'));
-    document.getElementById('btn-cau').addEventListener('click', () => copyToClipboard('sudo su postgres\npsql produccion_ca\nselect * from migracion.ver_bloqueos ;\n'));
-
-    // Agregar evento de escucha al teclado para los cuadros de texto de bloqueo
-    const inputs = document.querySelectorAll('.query-inputs input');
-    inputs.forEach(input => {
-        input.addEventListener('keydown', (event) => {
-            if (event.key === 'Enter') {
-                event.preventDefault(); // Evitar el comportamiento predeterminado del Enter en el cuadro de texto
-                generarYCopiarSentencias();
-            }
-        });
-    });
-
-    // Agregar un objeto para almacenar los nombres de las islas y sus bases de datos
-    const islasBasesDatos = {
-        GCA: 'produccion_lpa',
-        TFE: 'produccion_tfe',
-        PMI: 'produccion_pmi',
-        LZA: 'produccion_lza',
-        VAL: 'produccion_val',
-        SDQ: 'sd_produccion',
-        PUR: 'produccion_pr',
-        CAU: 'produccion_ca',
-    };
-
-    // Evento de clic para los números telefónicos en la tabla
-    const clickToCopyElements = document.querySelectorAll('.click-to-copy');
-    clickToCopyElements.forEach(element => {
-        element.addEventListener('click', () => {
-            const phoneNumber = element.dataset.phoneNumber;
-            copyToClipboard(phoneNumber);
-        });
-    });
-
-    document.getElementById('btn-generar').addEventListener('click', () => {
-        generarYCopiarSentencias();
-    });
-
-    document.addEventListener('DOMContentLoaded', () => {
-        const numbersToCopy = document.querySelectorAll('.click-to-copy');
-
-        numbersToCopy.forEach(number => {
-            number.addEventListener('click', () => {
-                const phoneNumber = number.dataset.phoneNumber;
-                copyToClipboardAndAlert(phoneNumber);
-            });
-        });
-    });
-
-function generarYCopiarSentencias() {
-    const sentenciasDesbloqueo = [];
-
-    // Obtener el valor seleccionado del desplegable
-    const islaSeleccionada = document.getElementById('islas-dropdown').value;
-
-    // Agregar la sentencia de bloqueo correspondiente según la isla seleccionada
-    const baseDatos = islasBasesDatos[islaSeleccionada];
-    if (baseDatos) {
-        sentenciasDesbloqueo.push(`sudo su postgres\npsql ${baseDatos}\nselect * from migracion.ver_bloqueos ;\n`);
-    }
-
-
-        agregarSentencia('input-01', '');
-        agregarSentencia('input-02', '');
-        agregarSentencia('input-03', '');
-        agregarSentencia('input-04', '');
-        agregarSentencia('input-05', '');
-        agregarSentencia('input-06', '');
-        agregarSentencia('input-07', '');
-        agregarSentencia('input-08', '');
-        agregarSentencia('input-09', '');
-        agregarSentencia('input-10', '');
-        agregarSentencia('input-11', '');
-        agregarSentencia('input-12', '');
-
-    if (sentenciasDesbloqueo.length > 0) {
-        const sentenciasCombinadas = sentenciasDesbloqueo.join('\n') + '\n';
-        copyToClipboard(sentenciasCombinadas);
-        alert('Sentencias de desbloqueo generadas y copiadas al portapapeles.');
-
-        // Vaciar los cuadros de texto después de copiar las sentencias
-        inputs.forEach((input) => (input.value = '')); // Establecer el valor a una cadena vacía
-    } else {
-        alert('No se ha ingresado ningún número en los cuadros de bloqueo.');
-    }
+/* Estilos para el contenedor principal */
+.container {
+    width: 100%;
+    max-width: 1000px;
+    margin: 0 auto; /* Centra el contenedor en la página */
+    padding: 20px;
+    background-color: #ffffff;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    text-align: center;
 }
 
-    // Función para copiar texto al portapapeles
-    function copyToClipboard(text) {
-        const el = document.createElement('textarea');
-        el.value = text;
-        document.body.appendChild(el);
-        el.select();
-        document.execCommand('copy');
-        document.body.removeChild(el);
-    }
+.container h1 {
+    font-size: 24px;
+    color: #007bff; /* Puedes elegir cualquier color que desees */
+    font-weight: bold;
+    margin-bottom: 10px;
+    font-family: 'Arial', sans-serif; /* Puedes cambiar la fuente según tus preferencias */
+}
 
+h2 {
+color: #007bff;
+    font-size: 24px;
+    font-family: 'Arial', sans-serif;
+    text-align: center;
+}
 
-// ... (Tu código JavaScript existente) ...
+.left-side h2 {
+    color: #007bff;
+    font-size: 24px;
+    font-family: 'Arial', sans-serif;
+    text-align: center;
+}
 
-// Agregar evento de escucha al teclado para el cuadro de texto de Verificar cuenta BBX
-const verifyInput = document.getElementById('verify-input');
-verifyInput.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        verificarCuentaBBX();
-    }
-});
-
-// Agregar evento de cambio para el desplegable de islas
-const islasDropdown = document.getElementById('islas-dropdown');
-islasDropdown.addEventListener('change', () => {
-    verificarCuentaBBX();
-});
-
-// ... (Tu código JavaScript existente) ...
-
-function verificarCuentaBBX() {
-    const nombreCuenta = verifyInput.value.trim();
-    if (nombreCuenta !== '') {
-        const selectedIsla = islasDropdown.value;
-        const sentenciasCombinadas = `sudo su postgres\npsql ${selectedIsla}\nselect * from migracion.ver_bloqueos ;\nselect nombre,created as fecha_creacion from ikea_erp.usuario where nombre = '${nombreCuenta}';\n`;
-        copyToClipboard(sentenciasCombinadas);
-
-        // Vaciar el cuadro de texto después de copiar las sentencias
-        verifyInput.value = '';
-    } else {
-
-    }
+.right-side h2 {
+    color: #007bff;
+    font-size: 24px;
+    font-family: 'Arial', sans-serif;
+    text-align: center;
 }
 
 
+
+/* Estilos para los botones de consulta */
+.query-btn {
+    display: flex;
+    justify-content: center; /* Alinea los botones hacia el inicio del contenedor */
+    flex-wrap: wrap;
+    margin-bottom: 20px; /* Espacio entre los botones y los cuadros de texto */
+}
+
+.query-btn button {
+    background-color: #007bff;
+    color: #ffffff;
+    border: none;
+    padding: 10px 15px;
+    font-size: 16px;
+    font-weight: bold;
+    border-radius: 5px;
+    cursor: pointer;
+    margin: 5px; /* Puedes ajustar este valor según el espacio deseado entre los botones */
+}
+
+
+button:active {
+    background-color: #ffcc00;
+    transition: background-color 0.3s;
+}
+
+.query-inputs {
+    margin-top: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+.query-inputs input {
+    width: 8ch;
+    padding: 10px;
+    margin: 5px;
+    font-size: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    text-align: center;
+    transition: font-size 0.2s;
+}
+
+.query-inputs input:focus {
+    font-size: 30px;
+}
+
+#btn-generar {
+    background-color: #007bff;
+    color: #ffffff;
+    border: none;
+    padding: 10px 20px;
+    font-size: 18px;
+    font-weight: bold;
+    border-radius: 5px;
+    cursor: pointer;
+    margin-top: 20px;
+}
+
+/* Estilos para la tabla */
+.left-side table, .right-side table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-bottom: 20px; /* Espacio entre las tablas y los botones de bloqueo */
+    background-color: #f9f9f9;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.left-side th, .left-side td, .right-side th, .right-side td {
+    padding: 8px;
+    border-bottom: 2px solid #ccc;
+}
+
+.left-side th, .right-side th {
+    background-color: #f0f0f0;
+}
+
+.left-side td, .right-side td {
+    text-align: center;
+}
+
+/* Estilos para los lados izquierdo y derecho */
+.left-side, .right-side {
+    flex: 1; /* Ambos lados ocupan el mismo espacio disponible */
+    text-align: left;
+    padding: 10px;
+    background-color: #f9f9f9;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+}
+
+/* Estilos para el contenedor de las tablas */
+.tables-container {
+    display: flex; /* Alinea las tablas como columnas */
+    justify-content: space-between; /* Espacio entre las tablas */
+    margin-bottom: 20px; /* Espacio entre las tablas y los botones de bloqueo */
+}
+/* Estilos para los números telefónicos en la tabla */
+.left-side td.click-to-copy {
+    white-space: pre; /* Mantiene los espacios en blanco y saltos de línea */
+    cursor: pointer;
+    color: #007bff; /* Color de enlace */
+    text-decoration: underline; /* Subrayado como enlace */
+}
+
+.left-side td.click-to-copy:hover {
+    color: #0056b3; /* Color de enlace al pasar el mouse */
+}
+
+table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-bottom: 20px;
+}
+
+th, td {
+    padding: 8px;
+    border-bottom: 2px solid #ccc;
+    text-align: center;
+}
+
+th {
+    background-color: #f0f0f0;
+}
+
+td span {
+    cursor: pointer;
+}
+
+td span:hover {
+    background-color: #f0f0f0;
+    cursor: pointer;
+}
+
+/* ... (Tu código CSS existente) ... */
+
+.verify-account {
+    margin-top: 20px;
+    text-align: center;
+}
+
+.verify-account h2 {
+    color: #007bff;
+    font-size: 24px;
+    font-family: 'Arial', sans-serif;
+}
+
+.verify-account input {
+    width: 400px;
+    padding: 10px;
+    margin-top: 10px;
+    font-size: 16px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    text-align: center;
+}
